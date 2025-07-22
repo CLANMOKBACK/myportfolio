7695ff6f15a8aa84faca2c841f0f1d85924ebdde
CLANMOKBACK <jjdeocampo20@gmail.com>
2025-07-22 02:30:26 -0700

    created qr code

--- generate_qr.py
+++ generate_qr.py
@@ -1,10 +1,7 @@
 import qrcode
 
-# URL for your portfolio
 url = "https://clanmokback.github.io/myportfolio/"
 
-# Generate QR code
 qr = qrcode.make(url)
 
-# Save as image
 qr.save("portfolio_qr.png")
